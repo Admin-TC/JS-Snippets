83c0f5be6822a2a337a6d795ae0214645427afe0
Tony Crofton <tony.crofton@hotmail.co.uk>
2022-01-16 21:41:10 +0000

    snippets

---
+++ daysleft.js
@@ -0,0 +1,15 @@
+var yourAge = prompt("Whats is your age human?");
+
+function lifeInWeeks(age){
+
+    var yearsLeft = 90 - age;
+
+    daysLeft = 365 * yearsLeft;
+    weeksLeft = 52 * yearsLeft;
+    monthsLeft = yearsLeft * 12;
+
+alert("Assuming you make it to 90 (hopefully!), you have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left");
+
+}
+
+lifeInWeeks(yourAge);
