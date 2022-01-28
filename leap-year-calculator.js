0eebe2aafd4ae7c1a058e81a9fb979cbc243eeea
Croftee <tony.crofton@hotmail.co.uk>
2022-01-28 11:58:14 +0000

    commit

---
+++ leap-year-calculator.js
@@ -0,0 +1,28 @@
+var isLeapyear = Boolean;
+var year = prompt("Hello human, please enter a year");
+var check1 = year % 100
+var check2 = year % 400
+
+function check1 ();
+{
+    if (check1 = 0)
+    {
+        check2();
+    }
+    else {
+        Alert(year + " is not a leap year");
+    }
+}
+
+function check2 ();
+{
+    if (check2 = 0)
+    {
+        Alert(year + " is a leap year!");
+    }
+
+    else {
+        Alert(year + " is not a leap year");
+    }
+}
+
