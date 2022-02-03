eccfb25c7e8cf3bc3bc5b48a111ef046d59f23e9
Tony Crofton <tony.crofton@hotmail.co.uk>
2022-02-03 21:47:23 +0000

    snippets

---
+++ fizzbuzz-refactored.js
@@ -0,0 +1,47 @@
+/* 
+
+The FizzBuzz problem is a classic test given in coding interviews. 
+
+The task is simple: 
+
+Print integers 1 to N, but print “Fizz” if an integer
+is divisible by 3, “Buzz” if an integer is divisible by 5, 
+and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+*/
+
+
+var output = [];
+var number = 1;
+
+function FizzBuzz()
+{
+        
+        if (number%3 == 0 && number%5 == 0)
+        {
+            output.push("FizzBuzz");
+            number++;
+        }
+        
+        else if (number%3 == 0)
+        {
+            output.push("Fizz");
+            number++;
+        }
+
+        else if (number%5 == 0)
+        {
+            output.push("Buzz");
+            number++;
+        }
+
+        else
+        {
+            output.push(number);
+            number++;
+        }   
+
+    return output
+}
+
+ console.log(output)
